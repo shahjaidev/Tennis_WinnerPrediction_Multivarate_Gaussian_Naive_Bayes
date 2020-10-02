e0b65a031b465dabf86df4925457972eea074f28
shahjaidev <js5161@columbia.edu>
2020-10-02 10:02:31 -0400

    Working predictor, plots

--- sportradar_wrapper.py
+++ sportradar_wrapper.py
@@ -3,25 +3,23 @@ import json
 
 import pandas as pd
 
+#h8w47gzf4ygv84jaa62jrb8k
 
-tennis_obj = Tennis.Tennis("ggy3z8e4qktyxz32jbft63my")
+tennis_obj = Tennis.Tennis("uzcb6y9ykvubwjcfw8rhrj2t")
 
 # Get a list of all tournaments
-daily_results = tennis_obj.get_daily_results(2019,10,10).json()
+daily_res_dict = tennis_obj.get_daily_results(2019,10,16).json()
 
-#print(daily_results)
+#print(type(daily_results_json_response))
+#daily_res_dict= json.loads(daily_results_json_response)
 
-json_file= open('daily_res.json')
-
-daily_res_dict= json.load(json_file)
 match_ids=[] 
 for match in daily_res_dict["results"]:
 
     if match["coverage_info"]["detailed_serve_outcomes"] is True:
         match_ids.append(match["sport_event"]["id"])
 
-with open("daily_res_2019_10_10.json", "w") as daily_results_file:
-    json.dump(daily_res_dict, daily_results_file)
+
 
 print(len(match_ids))
 dataset_li= []
@@ -29,7 +27,7 @@ dataset_li= []
 i=0
 
 for id in match_ids:
-    if i>= 90:
+    if i>= 100:
         break
     try:
         statistics_dict= tennis_obj.get_match_summary(id).json()
@@ -48,9 +46,11 @@ for id in match_ids:
 
         ace_difference= player_A_stats["aces"]- player_B_stats["aces"]
         double_fault_difference= player_A_stats["double_faults"]- player_B_stats["double_faults"]
-        #total_breakpoints_difference= player_A_stats["total_breakpoints"]- player_B_stats["total_breakpoints"]
+        total_breakpoints_difference= player_A_stats["total_breakpoints"]- player_B_stats["total_breakpoints"]
+        first_serve_successful_percent_difference= player_A_stats["first_serve_successful"]- player_B_stats["first_serve_successful"]
+        second_serve_successful_percent_difference= player_A_stats["second_serve_successful"]- player_B_stats["second_serve_successful"]
 
-        row= [ace_difference, double_fault_difference, output_winner]
+        row= [ace_difference, double_fault_difference,total_breakpoints_difference, first_serve_successful_percent_difference, second_serve_successful_percent_difference, output_winner]
         dataset_li.append(row)
         
     except:
@@ -62,7 +62,7 @@ print(dataset_li)
 df = pd.DataFrame(dataset_li)
 
 print("Saving the data...")
-writer = pd.ExcelWriter('tennis_2019_10_10.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter('tennis_2019_10_16.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='welcome', index=False)
 writer.save()
 
